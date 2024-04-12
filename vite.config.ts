28a6bec17b23dabc79ab21fa80d892db723d9eb6
kid <2713786149@qq.com>
2024-04-12 15:13:36 +0800

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   test: { environment: 'happy-dom' },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
