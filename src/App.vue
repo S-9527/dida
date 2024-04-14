e8454f71dd950cadfaafc32c8d3439539e9a9801
kid <2713786149@qq.com>
2024-04-14 19:31:28 +0800

    feat(ui): layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <router-view></router-view>
+  <NConfigProvider :theme="naiveTheme">
+    <router-view></router-view>
+  </NConfigProvider>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NConfigProvider, darkTheme } from 'naive-ui';
+import { computed } from 'vue';
+import { isDark } from './composable/dark';
+
+const naiveTheme = computed(() => isDark.value ? darkTheme : null);
+</script>
 
 <style scoped></style>
