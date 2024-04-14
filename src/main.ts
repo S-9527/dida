f798179fe87fc50d0392539c0013ff84efef753d
kid <2713786149@qq.com>
2024-04-14 18:52:58 +0800

    chore: migrate to unocss

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from "pinia";
-import './style.css'
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
 import App from './App.vue'
 import { router } from "./router";
 import ContextMenu from '@imengyu/vue3-context-menu'
+import "@unocss/reset/tailwind.css"
+import "uno.css"
 
 const app = createApp(App)
 const pinia = createPinia()
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: { environment: 'happy-dom' },
-  plugins: [vue()],
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
       '@': '/src'
