137c95eb07922ee98b2847dacf193a8063e1c918
kid <2713786149@qq.com>
2024-04-15 10:23:27 +0800

    chore: cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,9 @@
-import About from '../view/About.vue'
 import Task from '../view/Task.vue'
 import { createRouter, createWebHashHistory } from "vue-router";
 
 
 export const routes = [
     { path: "/", redirect: "/task" },
-    { path: "/about", component: About, name: "About" },
     { path: "/task", component: Task, name: "Task" },
 ];
 
