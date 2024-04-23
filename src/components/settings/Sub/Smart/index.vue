54fcdbe9b5646e9ab1f925d7e823b216a4a507e1
kid <2713786149@qq.com>
2024-04-23 22:31:21 +0800

    refactor: settings smart component

--- src/components/settings/Sub/Smart/index.vue
+++ src/components/settings/Sub/Smart/index.vue
@@ -1,65 +1,16 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
 import { NSelect } from 'naive-ui'
-import { reactive } from 'vue'
-import type { TaskListType } from '@/components/task/TaskLeftListSmartProject.vue'
-import { SmartProjectNames } from '@/service/task'
-import { useProjectMoreActions } from "@/composable/useProjectMoreActions.ts";
+import { useSettingsStore } from "@/store";
 
-const { hideTaskItem, showTaskItem, whetherCanShow } = useProjectMoreActions()
-
-
-const taskList = reactive<TaskListType[]>([
-  {
-    key: 1,
-    icon: 'material-symbols:check-box',
-    title: SmartProjectNames.Complete,
-    option: whetherCanShow(1) ? 'visible' : 'hidden',
-  },
-  {
-    key: 2,
-    icon: 'mdi:close-box',
-    title: SmartProjectNames.Failed,
-    option: whetherCanShow(2) ? 'visible' : 'hidden',
-  },
-  {
-    key: 3,
-    icon: 'material-symbols:delete',
-    title: SmartProjectNames.Trash,
-    option: whetherCanShow(3) ? 'visible' : 'hidden',
-  },
-  {
-    key: 4,
-    icon: 'material-symbols:text-snippet-rounded',
-    title: SmartProjectNames.Abstract,
-    option: whetherCanShow(4) ? 'visible' : 'hidden',
-  },
-])
-
-const options = [
-  {
-    label: '显示',
-    value: 'visible',
-  },
-  {
-    label: '隐藏',
-    value: 'hidden',
-  },
-]
-
-const handleChangeOption = (key: number, value: string) => {
-  if (value === 'visible')
-    showTaskItem(key)
-  if (value === 'hidden')
-    hideTaskItem(key)
-}
+const settingsStore = useSettingsStore()
 </script>
 
 <template>
   <ul p-8>
     <li
-        v-for="item in taskList"
-        :key="item.key"
+        v-for="(item, key) in settingsStore.settingsSmartProjects"
+        :key="key"
         class="pl-4 pr-2 h-60px flex items-center"
     >
       <div w-120px flex items-center>
@@ -74,8 +25,7 @@ const handleChangeOption = (key: number, value: string) => {
       <div w-200px>
         <NSelect
             v-model:value="item.option"
-            :options="options"
-            @update:value="handleChangeOption(item.key, $event)"
+            :options="item.options"
         />
       </div>
     </li>
