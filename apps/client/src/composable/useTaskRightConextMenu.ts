affbeb4e6d981417f13f8fa7f8a0068e504e3445
kid <2713786149@qq.com>
2024-05-05 18:15:30 +0800

    feat: add search task can be moved to other project

--- apps/client/src/composable/useTaskRightConextMenu.ts
+++ apps/client/src/composable/useTaskRightConextMenu.ts
@@ -1,12 +1,62 @@
 import ContextMenu from "@imengyu/vue3-context-menu";
 import { useTaskStore } from "@/store";
 import { useTaskOperationMessage } from "./useTaskOperationMessage.ts";
-import { h, toRefs } from "vue";
+import { h, reactive, toRefs } from "vue";
 
 export function useTaskRightContextMenu() {
     const { removeTask, moveTask } = useTaskStore();
     const { currentActiveTask, projects } = toRefs(useTaskStore());
     const { showRemoveMessage, showMoveMessage } = useTaskOperationMessage()
+
+    const moveProjects = [...getSearchMenuItem(), ...getMoveListProjects()]
+    const menuData = reactive({
+        x: 0,
+        y: 0,
+        items: [
+            {
+                label: '移动到',
+                children: moveProjects,
+            },
+            {
+                label: '删除',
+                onClick: () => {
+                    showRemoveMessage(currentActiveTask.value!)
+                    removeTask(currentActiveTask.value!)
+                },
+            },
+        ],
+    })
+
+    function getSearchMenuItem() {
+        return [{
+            label: h('input', {
+                style: {
+                    width: '100px',
+                    height: '20px',
+                    outlineColor: 'rgb(86, 125, 250)',
+                },
+                placeholder: '搜索',
+                id: 'searchInput',
+                onInput: changeInput,
+            }),
+            clickClose: false,
+        }]
+    }
+
+    function changeInput() {
+        const inputValue = (<HTMLInputElement>document.getElementById('searchInput')).value
+        if (!inputValue) {
+            menuData.items[0].children = moveProjects
+            return
+        }
+        menuData.items[0].children = moveProjects.filter((projectItem) => {
+            if (typeof projectItem.label === 'string') {
+                return (projectItem.label as string).includes(inputValue)
+            }
+
+            return true
+        })
+    }
 
     function getMoveListProjects() {
         return projects.value.map((projectItem) => {
@@ -35,23 +85,10 @@ export function useTaskRightContextMenu() {
 
     function showContextMenu(e: MouseEvent) {
         e.preventDefault();
-        ContextMenu.showContextMenu({
-            x: e.x,
-            y: e.y,
-            items: [
-                {
-                    label: '移动到',
-                    children: [...getMoveListProjects()],
-                },
-                {
-                    label: "删除",
-                    onClick: () => {
-                        showRemoveMessage(currentActiveTask.value!)
-                        removeTask(currentActiveTask.value!)
-                    },
-                },
-            ],
-        });
+        menuData.x = e.x
+        menuData.y = e.y
+        menuData.items[0].children = moveProjects
+        ContextMenu.showContextMenu(menuData)
     }
 
     return {
