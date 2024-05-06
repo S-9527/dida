d5699db5ac2f824d5197965deebc44363e6f8c63
kid <2713786149@qq.com>
2024-05-07 00:42:54 +0800

    refactor: command view

--- apps/client/src/components/navbar/Navbar.vue
+++ apps/client/src/components/navbar/Navbar.vue
@@ -1,36 +1,57 @@
 <script setup lang="ts">
 import { NDropdown, NPopover } from 'naive-ui'
-import { ref } from 'vue'
 import Command from '@/components/command/Command.vue'
-import { useIsMac } from "@/store/useCommandStore.ts";
+import type { DropdownMixedOption } from 'naive-ui/es/dropdown/src/interface'
+import { useCommand } from "@/components/command/command.ts";
+import { useGoto } from "@/components/header/theHeader.ts";
 
-const commandRef = ref<InstanceType<typeof Command>>()
-const isMac = useIsMac()
+const { showCommand } = useCommand()
+const { gotoSettings } = useGoto()
 
-// Command + K Or Command + / will show command in MacOS
-// Ctrl + K Or Ctrl + / in Windows
-window.addEventListener('keydown', (e: KeyboardEvent) => {
-  if ((e.key === 'k' || e.key === 'л') && (isMac.value ? e.metaKey : e.ctrlKey)) {
-    e.preventDefault()
-    commandRef.value?.show()
-  }
-  if ((e.key === '/' || e.key === ',') && (isMac.value ? e.metaKey : e.ctrlKey)) {
-    e.preventDefault()
-    commandRef.value?.show()
-  }
-})
+const settingOptions: DropdownMixedOption[] = [
+  {
+    label: '设置',
+    key: 'settings',
+    props: {
+      onClick: () => {
+        gotoSettings()
+      },
+    },
+  },
+]
+
+interface ActivityIconOption {
+  label: string
+  icon: string
+  onClick?: () => void
+}
+
+const activityIconOptions: ActivityIconOption[] = [
+  {
+    label: '任务',
+    icon: 'carbon-checkbox-checked-filled',
+  },
+  {
+    label: '日历',
+    icon: 'carbon-calendar-heat-map',
+  },
+  {
+    label: '打卡',
+    icon: 'carbon-task-complete',
+  },
+  {
+    label: '搜索面板',
+    icon: 'carbon-search',
+    onClick() {
+      showCommand()
+    },
+  },
+]
 </script>
 
 <template>
   <div class="w-[48px] border-r border-blue border-solid py-2 dark:bg-black">
-    <NDropdown
-        placement="right-end"
-        :options="[
-            {
-              label: '设置',
-              key: 'settings',
-            }
-        ]"
+    <NDropdown placement="right-end" :options="settingOptions"
     >
       <!-- avatar -->
       <div class="w-[32px] h-[32px] rounded-sm bg-blue mx-auto">
@@ -39,44 +60,19 @@ window.addEventListener('keydown', (e: KeyboardEvent) => {
     </NDropdown>
     <!-- page router -->
     <div class="text-gray-4 flex flex-col justify-center items-center text-xl mt-10 gap-6 font-bold">
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-              class="hover:text-gray-6 dark:hover:text-gray-2"
-              i="carbon-checkbox-checked-filled"
-          />
-        </template>
-        <span>任务</span>
-      </NPopover>
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-              class="hover:text-gray-6 dark:hover:text-gray-2"
-              i="carbon-calendar-heat-map"
-          />
-        </template>
-        <span>日历</span>
-      </NPopover>
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-              class="hover:text-gray-6 dark:hover:text-gray-2"
-              i="carbon-task-complete"
-          />
-        </template>
-        <span>打卡</span>
-      </NPopover>
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-              class="hover:text-gray-6 dark:hover:text-gray-2 cursor-pointer"
-              i="carbon-search"
-              @click="commandRef?.show()"
-          />
-        </template>
-        <span>搜索面板</span>
-      </NPopover>
+      <template v-for="(iconOption, i) in activityIconOptions" :key="i">
+        <NPopover placement="right">
+          <template #trigger>
+            <div
+                class="hover:text-gray-6 dark:hover:text-gray-2"
+                :i="iconOption.icon"
+                @click="() => iconOption.onClick && iconOption.onClick()"
+            />
+          </template>
+          <span>{{ iconOption.label }}</span>
+        </NPopover>
+      </template>
     </div>
-    <Command ref="commandRef" />
+    <Command/>
   </div>
 </template>
