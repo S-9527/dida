479911872f2e8176ac72fb4e23a293ef39644e55
kid <2713786149@qq.com>
2024-05-07 23:10:46 +0800

    chore: add cats demo for mongoDB

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { CatsModule } from "./cats/cats.module";
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/dida'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
