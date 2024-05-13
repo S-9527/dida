8cd77293df0e3b8f9bf3d51082e816e6889f5dee
kid <2713786149@qq.com>
2024-05-14 06:04:45 +0800

    refactor: remove search input

--- apps/client/src/composables/useTaskRightConextMenu.ts
+++ apps/client/src/composables/useTaskRightConextMenu.ts
@@ -1,8 +1,8 @@
 import ContextMenu from "@imengyu/vue3-context-menu";
-import type { MenuOptions } from '@imengyu/vue3-context-menu'
+import type { MenuItem, MenuOptions } from '@imengyu/vue3-context-menu'
 import { useListProjectsStore, useTasksStore } from "@/store";
 import { useTaskOperationMessage } from "./useTaskOperationMessage.ts";
-import { h, reactive, toRefs } from "vue";
+import { reactive, toRefs } from "vue"
 
 export function useTaskRightContextMenu() {
     const { moveTaskToProject, removeTask } = useTasksStore();
@@ -10,94 +10,53 @@ export function useTaskRightContextMenu() {
     const { showRemoveMessage, showMoveMessage } = useTaskOperationMessage()
     const listProjectsStore = useListProjectsStore()
 
-    const moveProjects: NonNullable<MenuOptions['items']> = [
-        ...getSearchMenuItem(),
-        ...getMoveListProjects()
-    ]
+    function createMenuData(x: number, y: number): any {
+        function createMoveItem(): MenuItem {
+            function getMoveListProjects() {
+                return listProjectsStore.projects
+                    .filter((project) => {
+                        return currentActiveTask?.value!.projectId !== project.id
+                    })
+                    .map((project) => {
+                        return {
+                            label: project.name,
+                            onClick: () => {
+                                showMoveMessage(project.name)
+                                moveTaskToProject(
+                                    currentActiveTask.value!,
+                                    project.id,
+                                )
+                            },
+                        }
+                    })
+            }
 
-    const menuData = reactive<MenuOptions>({
-        x: 0,
-        y: 0,
-        items: [
-            {
+            return {
                 label: '移动到',
-                children: moveProjects,
-            },
-            {
+                children: [...getMoveListProjects()],
+            }
+        }
+
+        function createRemoveItem(): MenuItem {
+            return {
                 label: '删除',
                 onClick: () => {
-                    showRemoveMessage(currentActiveTask!)
-                    removeTask(currentActiveTask!)
+                    showRemoveMessage(currentActiveTask.value!)
+                    removeTask(currentActiveTask.value!)
                 },
-            },
-        ],
-    })
-
-    function getSearchMenuItem() {
-        return [{
-            label: h('input', {
-                style: {
-                    width: '100px',
-                    height: '20px',
-                    outlineColor: 'rgb(86, 125, 250)',
-                },
-                placeholder: '搜索',
-                id: 'searchInput',
-                onInput: changeInput,
-            }),
-            clickClose: false,
-        }]
-    }
-
-    function changeInput() {
-        const inputValue = (<HTMLInputElement>document.getElementById('searchInput')).value
-        if (menuData.items) {
-            if (!inputValue) {
-                menuData.items[0].children = moveProjects
-                return
             }
-            menuData.items[0].children = moveProjects.filter((projectItem) => {
-                if (typeof projectItem.label === 'string') {
-                    return (projectItem.label as string).includes(inputValue)
-                }
-
-                return true
-            })
         }
-    }
 
-    function getMoveListProjects() {
-        return listProjectsStore.projects.map((project: any) => {
-            const isCurrentProject = currentActiveTask.value?.projectId === project.id
-            return {
-                label: isCurrentProject
-                    ? h(
-                        'span',
-                        {
-                            style: {
-                                color: '#567dfa',
-                            },
-                        },
-                        project.name,
-                    )
-                    : project.name,
-                onClick: () => {
-                    if (isCurrentProject) return
-                    showMoveMessage(project.name)
-                    moveTaskToProject(currentActiveTask!, project.id)
-                },
-            }
+        return reactive<MenuOptions>({
+            x,
+            y,
+            items: [createMoveItem(), createRemoveItem()],
         })
     }
 
     function showContextMenu(e: MouseEvent) {
         e.preventDefault();
-        menuData.x = e.x
-        menuData.y = e.y
-        if (menuData.items) {
-            menuData.items[0].children = moveProjects
-        }
-        ContextMenu.showContextMenu(menuData)
+        ContextMenu.showContextMenu(createMenuData(e.x, e.y))
     }
 
     return {
