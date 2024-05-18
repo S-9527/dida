5f36653953b9297f9d665ca9839cff550e52013f
kid <2713786149@qq.com>
2024-05-18 16:42:59 +0800

    test: add indexeddb for test env

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,10 +1,14 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: { environment: 'happy-dom' },
+  test: {
+    environment: 'happy-dom',
+    setupFiles: path.resolve(__dirname, './vitest.setup.ts'),
+  },
   plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
